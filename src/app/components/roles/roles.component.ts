3902ce5a84d1558b935c01d817793f0fdc8c8ff1
richieArnold <arnoldrichie31.rar@gmail.com>
2024-12-09 10:18:17 +0530

    add services

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { APIResponseModel, IRole } from '../../model/interface/role';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css',
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit {
+  // constructor(private http: HttpClient) {
+  // this is old way called dependency injection
+  // }
+  roleList: IRole[] = [];
+  http = inject(HttpClient);
+  ngOnInit(): void {
+    this.getAllRoles();
+  }
+
+  getAllRoles() {
+    this.http
+      .get<APIResponseModel>(
+        'https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllRoles'
+      )
+      .subscribe((res: any) => {
+        console.log(res);
+        this.roleList = res.data;
+      });
+  }
   firstName: string = 'Angular Project';
   angularVersion: string = 'Version 19';
   version: number = 19;
